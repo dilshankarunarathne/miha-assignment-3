eb7efceba7ea3de570616874f28810b938d22de1
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-05 09:39:49 +0530

    Working on

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-mock_db = {}
+mock_db = []
 
 
 def populate():
@@ -7,7 +7,8 @@ def populate():
         if line is None:
             pass
         nic_1, nic_2, contact_date = line.split(",")
-        mock_db[nic_1] = nic_2, contact_date
+        mock_db.append(nic_1, nic_2, contact_date)
+        mock_db.append(nic_2, nic_1, contact_date)
 
 
 if __name__ == '__main__':
