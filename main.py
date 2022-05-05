2020043e3b09de430eaa8a12fc9e956a3a0f158f
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-05 10:26:52 +0530

    Working on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 mock_db = []
+to_quarantine = []
 
 
 def populate():
@@ -15,15 +16,24 @@ def sorted_dict():
     mock_db.sort(key=lambda x: x[2])
 
 
-def print_list(nic):
+def create_list(nic):
+    to_check = [nic]
     for contact in mock_db:
         nic_1, nic_2, contact_date = contact
-        if nic_1 == nic:
-            print(nic_2)
+        if nic_1 in to_check:
+            to_check.append(nic_2)
+            to_quarantine.append(nic_2)
+
+
+def print_list():
+    for infected_person_nic in set(sorted(to_quarantine)):
+        print(infected_person_nic)
 
 
 if __name__ == '__main__':
     populate()
+    sorted_dict()
     # print(mock_db)
     c_person_nic, c_contact_date = str(input()).split(" ")
-    print_list(c_person_nic)
+    create_list(c_person_nic)
+    print_list()
